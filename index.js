20710c467bf0e125fa31bbc93263cd27f74f7e7e
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 01:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Satya"
-var height = "66"
+var height = 66
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height)=parseInt(height) + 1
